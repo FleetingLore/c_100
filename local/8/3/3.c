d31f32f643576e64851234201b460708846d4046
Fleeting <Macro__9@outlook.com>
2025-11-08 19:02:32 +0800

    0

---
+++ local/8/3/3.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+#define N 40
+
+int ReadScore(int score[]);
+int FindMax(int score[], int n);
+
+int main() {
+    int score[N], max, n;
+    n = ReadScore(score);
+    printf("Total students are %d\n", n);
+    max = FindMax(score, n);
+    printf("The heighest score is %d\n", max);
+
+    return 0;
+}
+
+int ReadScore(int score[]) {
+    int i = -1;
+    do {
+        i += 1;
+        printf("Input score:");
+        scanf("%d", score[i]);
+    } while (score[i] >= 0);
+
+    return i;
+}
+
+int FindMax(int score[], int n) {
+    int max = score[0];
+    for(int i = 1; i < n; i++) {
+        if (score[i] > max) {
+            max = score[i];
+        }
+    }
+
+    return max;
+}
